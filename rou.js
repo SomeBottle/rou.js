c43fb3cfcde23c826e862d97f048a295831fedc3
SomeBottle <583797659@qq.com>
2020-02-02 11:10:21 +0800

    Just route~

---
+++ rou.js
@@ -0,0 +1,89 @@
+var rou = {
+	ps:{def:{},cg: {}},
+    /*routes*/
+    statu: false,
+    /*ifrun*/
+    id: 0,
+    el: '',
+    /*element*/
+    c: function() { /*checker*/
+	    var o = this,u = window.location.href,rq = o.sp(u),hk=o.ps['def']['ky'];/*homekey*/
+        if (u.indexOf('#') !== -1 && rq !== hk) {
+            var ctg = rq.substring(0, 1),k = rq.replace(ctg, ''),r = o.j(ctg,k)['c'],i = o.j(ctg,k)['id'];
+            if (typeof r == 'function') {
+                r(k, i); /*(page,id)*/
+            } else if(r !== ''){
+                o.el.innerHTML = r;
+            }
+        } else if (hk) {
+            var r = o.ps['def']['c'],i = o.ps['def']['id'];
+			if(!rq) window.location.href+='#'+hk;
+            if (typeof r == 'function') {
+                r('home', i); /*(page,id)*/
+            } else {
+                o.el.innerHTML = r;
+            }
+        }
+    },
+	j:function(c,k){
+		var mc=this.ps['cg'][c];
+		const b=this.ps['cg'][c] || {[k]:{c:'',i:''}};
+		return b[k]||{c:'',i:''};
+	},
+    x: function(e) { /*elementselector*/
+        this.el = document.getElementById(e);
+		return this;
+    },
+    sp: function(y) { /*splitter*/
+        var s = y.split('?');
+        return (s[0].split('/#'))[1];
+    },
+    r: function() { /*run/stop*/
+        var o = this;
+        if (!o.statu) {
+            window.addEventListener('hashchange', o.c.bind(o));
+            o.c();
+            o.statu = true;
+        } else {
+            window.removeEventListener('hashchange', o.c.bind(o));
+            o.statu = false;
+        }
+		return o;
+    },
+    a: function(t, k, ct, c = false,i = false) { /*add(type,key,content(html/func),category,id)*/
+        var o = this;
+        if (!i) {
+            i = o.id;
+            o.id += 1;
+        }
+        if (!c) {
+            c = '!';
+        }
+        if (t == 'def') {
+            o.ps['def'] = {
+				ky:k,
+                id: i,
+                c: ct
+            };
+        } else if (t == 'reg') {
+            if(!o.ps['cg'][c]) o.ps['cg'][c]={};
+            o.ps['cg'][c][k] = {
+                id: i,
+                c: ct
+            };
+        }
+        return o;
+    },
+    d: function(t, k, c = false) { /*del(type,key,category)*/
+        var o = this;
+        if (!c) {
+            c = '!';
+        }
+        if (t == 'def') {
+            o.ps['def'] = {};
+        } else if (t == 'reg') {
+            delete o.ps['cg'][c][k];
+        }
+        return o;
+    }
+};
